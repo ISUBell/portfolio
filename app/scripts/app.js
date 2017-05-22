c2a34d812d42b72c48e7b3ad5a999118273bbab5
Michael Bell <mbelljunk@gmail.com>
2017-05-21 19:04:16 -0500

    =initialized viewport routing and setup ui-router

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,7 +1,19 @@
 'use strict';
 
-angular.module('portfolioApp', [])
-  .config(function() {
-  //setup states here
-})
-;
+angular.module('portfolioApp', ['ui.router'])
+  .config(function($stateProvider) {
+  
+  var aboutState = {
+    name: 'about',
+    url: '/',
+    template: '<p>About state template from app.js</p>'
+  };
+  
+  $stateProvider.state(aboutState);
+
+  $stateProvider.state('aboutURL', {
+    url: '/about',
+    templateUrl: '../views/about.html'
+  })
+  
+});
