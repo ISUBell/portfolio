64d24de595e95e82747e6231b064e571ccdb0b99
Michael Bell <mbelljunk@gmail.com>
2017-06-07 21:20:25 -0500

    =Added more content to about and examples pages

--- app/scripts/services/services.js
+++ app/scripts/services/services.js
@@ -10,16 +10,29 @@ angular.module('portfolioApp')
     
     var projects = [
       {
-        name:'portfolio',
+        name:'This Website',
         image: '',
         category:'html',
-        description: 'A sample website'
+        description: 'This website was built using AngularJS, and uses a single page application design. '
+      },
+      {
+        name:'Chat app',
+        image:'',
+        category:'html',
+        description: 'NodeJS application that features a live chatroom. By following the below link you can access a live version of the application',
+        github: ''
+      },
+      {
+        name:'Todo app',
+        image: '',
+        category: 'html',
+        description: 'NodeJS application that will perform basic CRUD operations via a command line interface'
       },
       {
         name: 'database app',
         image: '',
         category: 'java',
-        description: 'Built using spring MVC and Java'
+        description: 'Built using Spring MVC and Java. Uses MySQL to interact with the database.'
       }
     ];
     
