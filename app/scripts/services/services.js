7c04d49d7f44d4c889479e0d4cbd7b4cfa1c177d
Michael Bell <mbelljunk@gmail.com>
2017-06-11 16:17:18 -0500

    =added some images and updated project descriptions

--- app/scripts/services/services.js
+++ app/scripts/services/services.js
@@ -11,9 +11,10 @@ angular.module('portfolioApp')
     var projects = [
       {
         name:'This Website',
-        image: '',
+        image: '../images/pic_angular.jpg',
         category:'html',
-        description: 'This website was built using AngularJS, and uses a single page application design. '
+        description: 'This website was built using AngularJS, and uses a single page application design. This site was built using a variety of web development tools, including NPM scripts, Bootstrap, Git, JSHint, and lite-server. A non-minified/uglified version is available via the Github link below.',
+        github:'https://github.com/ISUBell/portfolio'
       },
       {
         name:'Chat app',
@@ -24,15 +25,18 @@ angular.module('portfolioApp')
       },
       {
         name:'Todo app',
-        image: '',
+        image: "../images/GETtodos.PNG",
         category: 'html',
-        description: 'NodeJS application that will perform basic CRUD operations via a command line interface'
+        description: 'NodeJS API that will perform CRUD operations on a NoSQL database. This takes the form of various todo\'s, that are grouped by registered users. The login for each user uses an encrypted and hashed password. This API is currently live on Heroku, and can be accessed via Postman or a similar method. The screenshots show a variety of calls and responses made to the live API. For testing out the API, make calls to the below website.',
+        website: 'https://sleepy-savannah-84383.herokuapp.com/',
+        github:'https://github.com/ISUBell/node-todo-api'
       },
       {
-        name: 'database app',
-        image: '',
+        name: 'Sports Statistics Database',
+        image: '../images/main page.png',
         category: 'java',
-        description: 'Built using Spring MVC and Java. Uses MySQL to interact with the database.'
+        description: 'Built using a variety of modern tools, including Spring MVC, Hibernate, JDBC, Maven, and others. Parses college basketball statistics from a web resource into a cloud SQL database which is then displayed to the user.',
+        github:'https://github.com/ISUBell/SportsTeamStatisticsDatabase'
       }
     ];
     
