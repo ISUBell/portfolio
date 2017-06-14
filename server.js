94f72e70b5a4ec13472679f7e3ec0ee6952d9b96
IowaState-Bell <mbell.isu@gmail.com>
2017-06-14 14:17:23 -0500

    =Added basic Google Analytics functionality

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
 var angular = require('angular')
-require('angulartics') // until we package it as a CommonJS module
+require('angulartics')
 var googleanalytics = require('angulartics-google-analytics')
 
 angular.module('myApp', ['angulartics', googleanalytics])
